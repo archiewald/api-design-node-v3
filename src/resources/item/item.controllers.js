352c88173b5a5e49b82027efe77c309689cdfbb4
archiewald <artur.kozubek1@gmail.com>
2019-10-01 20:42:32 +0200

    api connection

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,19 @@
+import { Item } from './item.model'
+import mongoose from 'mongoose'
+import { connect } from '../../utils/db'
+
+const run = async () => {
+  await connect('mongodb://localhost:27017/api-test')
+
+  const item = await Item.create({
+    name: 'Make coffee',
+    createdBy: mongoose.Types.ObjectId(),
+    list: mongoose.Types.ObjectId()
+  })
+
+  console.log(item)
+}
+
+run()
+
 export default {}
